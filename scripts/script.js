82b7798e1b6fffaae5f0d2528f9da015ae7d4da5
Michael Baldwin <mbaldwin08@gmail.com>
2018-02-23 11:33:14 -0700

    Update navbar transition scrollTop() > 0

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-        // Transition effect for navbar 
-        $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > document.documentElement.clientHeight * .95) { 
-              $('#scroll-sticky').addClass('nav-transition');
-          } else {
-              $('#scroll-sticky').removeClass('nav-transition');
-          }
-        });
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 0) { 
+          $('#scroll-sticky').addClass('nav-transition');
+      } else {
+          $('#scroll-sticky').removeClass('nav-transition');
+      }
+    });
 });
